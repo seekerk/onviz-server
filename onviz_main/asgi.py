b5339c54586ef08edb74a7661daaf8852c125c16
Kirill Kulakov <kulakov@cs.petrsu.ru>
2024-04-08 16:21:33 +0300

    Шаблон проекта

---
+++ onviz_main/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for onviz_main project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onviz_main.settings')
+
+application = get_asgi_application()
